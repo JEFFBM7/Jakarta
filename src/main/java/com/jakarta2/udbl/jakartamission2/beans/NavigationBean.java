0a3deac3bb8d9e454bbeec53dfff55c40ddd10a4
JEFFBM7 <votre-email@example.com>
2026-01-14 17:51:35 +0100

    Mise à niveau de Java 11 vers Java 21

--- src/main/java/com/jakarta2/udbl/jakartamission2/beans/NavigationBean.java
+++ src/main/java/com/jakarta2/udbl/jakartamission2/beans/NavigationBean.java
@@ -24,6 +24,7 @@ import java.util.List;
 @SessionScoped
 public class NavigationBean implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
     
     @Inject
